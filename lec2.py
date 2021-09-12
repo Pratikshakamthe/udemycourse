a96a131808228fa4fab33ef3050886940354569e
Pratikshakamthe <pratikshakamthe18@gmail.com>
2021-09-12 20:15:17 +0530

    commit

--- lec2.py
+++ lec2.py
@@ -1,3 +1,7 @@
 #Day 1 - Python Print Functions
 #The Function is declared like this:
 #print("What to print")
+
+print("Day 1 - Python Print Functions")
+print("The Function is declared like this:")
+print('print("What to print")')
